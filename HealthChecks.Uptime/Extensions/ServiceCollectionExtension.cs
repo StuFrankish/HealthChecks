fb9ca978fde40d939aae883a1925b5a54fff6e75
Stu Frankish <contact@stufrankish.dev>
2024-02-26 20:27:45 +0000

    Update package

---
+++ HealthChecks.Uptime/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace HealthChecks.Uptime;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddApplicationUptimeHealthCheck(this IServiceCollection services)
+    {
+        services.TryAddSingleton<StartupTimeHealthCheck>();
+
+        services.AddHealthChecks()
+            .AddCheck<StartupTimeHealthCheck>(name: "startup_time");
+
+        return services;
+    }
+}
